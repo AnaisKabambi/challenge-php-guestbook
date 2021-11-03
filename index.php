b05edf01817474b3109670c553cc1b0f4d638fd1
Anais Kabambi <anais.k@hotmail.be>
2021-11-03 11:40:51 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Guestbook</title>
+</head>
+<body>
+<form>
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname"><br>
+    <label for="message">Message:</label><br>
+    <input type="text" id="message" name="message">
+</form>
+</body>
+</html>
